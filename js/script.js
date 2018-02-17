f42b2fa3fee42b29c11a7a4265547192d0217308
Marta Waplak <mwaplak@gmail.com>
2018-02-17 11:45:58 +0100

    Upload jquery slideshow

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(function(){
+    var width = 450;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+    
+    var $carousel = $('#carousel');
+    var $slideContainer = $carousel.find('.slides');
+    var $slides = $slideContainer.find('.slide');
+    
+    var interval;
+    
+    function startSlider() {
+        interval = setInterval(function() {
+            $('.slides').animate({'margin-left': '-=' + width}, animationSpeed, function() {
+            currentSlide++;
+                if(currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    
+    function stopSlider () {
+        clearInterval(interval);
+    }
+    
+    $carousel.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+    
+    startSlider();
+                 
+});
